d0bf2b731447ca1c29dd95dc6cf7e937f44923f8
teo <teogallarza@gmail.com>
2019-09-19 11:33:11 -0300

    did some work on main class

--- grafo.py
+++ grafo.py
@@ -1,80 +1,69 @@
-from typing import Iterable
 
-
-vertices =[]
-'''
-def __input_lines__(n: int, ent: ) -> Iterable[str]:
-    for _ in range(n):
-        yield input()
-'''
+## tem que pensar especificamente sobre a estrutura de dados que vai ser usada para os pesos e para as arestas
+##poderia ser um dict q aponta p uma lista(vizinhos) d tuplas = (w,v) ou qlq coisa assim
 
 class Grafo:
-#    def __init__(self, v, e, w):
-#        self.v = v # conjunto de vertices
-#        self.e = e # conjunto de arestas
-#        self.w = w # w : E -> R, a funcao que mapeia o peso de cada aresta {u,v} pertencentes a E
+    arestas = {}
+    vertices = {}
 
     def qtdVertices(self):
         return len(v)
 
     def qtdArestas(self):
-        return len(e)
+        return len(self.arestas)
 
     def grau(self, v):
         return v
 
-    def ler(self, arquivo):
+
+
+    def __init__(self):
+        arquivo = input()
         r = open(arquivo, "r")
         ent = r.read().splitlines()
-        #ent = data.
-        #entrada = input().split() #primeira entrada com vertices n
-        #ent = r.readline().split() #ent é entrada
         n = int(ent[0].split()[1])
-        #vertices_data = [l.split() for l in ent[n]]
-        #print(vertices_data)
 
-        #vertices_data = (l.split() for l in __input_lines__(n, ent))  # pega os vértices com seus labels
-        #nomes_dict = {x: y for x, y in vertices_data}        # cria um dict com {vertice : label}
-        #nomes_dict = {x: y for x, y in ent[n].split() for n in range(1,n)}
-        #nomes_dict = {x: y for x, y in e.split() for e in ent for }
         print(ent[1:n+1])
-        #nomes_dict = {x:y for e in ent[1:n+1] for x, y in e.split()}
-        #nomes_dict = {x:y for x,y in ent[1:n+1].split()}
+
         nomes_dict = {x: y for x, y in map(lambda e: e.split(), ent[1:n+1])}
-        print(nomes_dict)
+        print("nomes = {}".format(nomes_dict))
 
         #eu não sei como fazer esse comprehension de cima /\
-
-        ''' parte inutil sem usar realline ou input
-        #_ = input()  #para pular o "*edges" no arquivo
-        _ = r.readline()  #para pular o "*edges" no arquivo
-        '''
 
         n += 2
         peso_dict = {} #criação de dicionarios para guardar os pesos
         arestas = {}   #e as arestas
-        while True:
-            try:
-                #entrada = input().split()
-                #entrada = r.readline().split()
-                entrada = ent[n].split()
-                u = entrada[0]
-                v = entrada[1]
-                weight = int(entrada[2])
+        for x in range(n,len(ent)):
+            entrada = ent[x].split()
+            print(entrada)
+            u = entrada[0]
+            v = entrada[1]
+            weight = int(entrada[2])
 
-                if u in arestas: # inserção {a:b}
-                    arestas[u]=arestas[u]+[v] # caso já exista chave
-                else:
-                    arestas[u]=[v] #caso não há a chave ainda
+            if u in arestas: # inserção {a:b}
+                arestas[u]=arestas[u]+[v] # caso já exista chave
+            else:
+                arestas[u]=[v] #caso não há a chave ainda
 
 
-                if v in arestas: # inserção {b:a}
-                    arestas[v]=arestas[v]+[u] # caso já exista a chave
-                else:
-                    arestas[v]=[u] #caso não há a chave ainda
+            if v in arestas: # inserção {b:a}
+                arestas[v]=arestas[v]+[u] # caso já exista a chave
+            else:
+                arestas[v]=[u] #caso não há a chave ainda
 
-            except EOFError as e:
-                break
-        print(peso_dict)
-        print(nomes_dict)
-    #colocar aqui a leitura do arquivo e talvez retirar o __init__
+        self.pesos = peso_dict
+        print("pesos = {}".format(peso_dict))
+        self.nomes = nomes_dict
+        self.arestas = arestas
+
+g = Grafo()
+print(g.qtdArestas())
+
+#X qtdVertices():  retornr a quantidade de v ́ertices;
+#X qtdArestas():  retorna a quantidade de arestas;
+#grau(v):  retorna o grau do v ́erticev;
+#rotulo(v):  retorna o r ́otulo do v ́erticev;
+#vizinhos(v):  retorna os vizinhos do v ́erticev;
+#haAresta(u, v):  se{u, v}∈E, retorna verdadeiro; se n ̃ao existir, retorna falso;
+#peso(u, v):  se{u, v}∈E, retorna o peso da aresta{u, v}; se n ̃ao existir, retorna um valor infinito positivo1;
+#ler(arquivo)2:  deve carregar um grafo a partir de um arquivo no formato especificado ao final deste docu-mento.
