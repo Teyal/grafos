009a7d4800e2f0854f38a0c2edd4f8135a3d2c44
Teo H. Gallarza <teogallarza@gmail.com>
2019-10-25 15:42:49 -0300

    quase terminado a 1

--- t2/1-CFC(nao ta completa).py
+++ t2/1-CFC(nao ta completa).py
@@ -4,12 +4,14 @@ import copy
 
 g = Grafo(True, False)
 
-print(g.arestas)
-print(g.transposta())
-
-print("Hello World")
-
 #  1-CFC
+
+def reset():
+    visitado = {x:False for x in g.vertices} # Cv←false ∀v ∈ V
+    tempoVisita = {x:float('inf') for x in g.vertices} # Tv ←∞ ∀v ∈ V
+    tempoFinal = {x:float('inf') for x in g.vertices} # Fv ←∞ ∀v ∈ V
+    antecessor = {x:None for x in g.vertices} # Av ←∞ ∀v ∈ V
+    return (visitado, tempoVisita, antecessor, tempoFinal)
 
 def vizinhos(d, v):
         return d[v]
@@ -23,11 +25,12 @@ def DFS_Visit(g, v, visitado, tempoVisita, antecessor, tempoFinal, tempo):
         for u in g.vizinhos(v): 
             if visitado[u] == False:
                 antecessor[u] = v
-                DFS_Visit(g, u, visitado, tempoVisita, antecessor, tempoFinal, tempo)
+                tempo = DFS_Visit(g, u, visitado, tempoVisita, antecessor, tempoFinal, tempo)
     
     
     tempo = tempo + 1
     tempoFinal[v] = tempo
+    return tempo
 #
 
 def DFS(g, visitado, tempoVisita, antecessor, tempoFinal):
@@ -36,50 +39,40 @@ def DFS(g, visitado, tempoVisita, antecessor, tempoFinal):
     
     for u in g.vertices:
         if visitado[u] == False:
-            DFS_Visit(g, u, visitado, tempoVisita, antecessor, tempoFinal, tempo)
+            tempo = DFS_Visit(g, u, visitado, tempoVisita, antecessor, tempoFinal, tempo)
                 
 #    return(visitado, tempoVisita, antecessor, tempoFinal)
 #
 
 # algoritmo auxiliar DFS-Alterado
-def DFS_Alterado(gTransposto, visitado, tempoVisita, antecessor, tempoFinal):
+def DFS_Alterado(gTransposto, vertices, visitado, tempoVisita, antecessor, tempoFinal):
     
     tempo = 0
-    # pega em ordem decrescente de F
-    teste = True
-    while (teste):
-        teste = False
-        maior = -1
-        maior_indice = -1
-        for i in g.vertices:
-            if tempoFinal[i] > maior and visitado[i] == False:
-                teste = True
-                maior = tempoFinal[i]
-                maior_indice = i
-        
-        if maior_indice != -1:
-            DFS_Visit(gTransposto, maior_indice, visitado, tempoVisita, antecessor, tempoFinal, tempo)
-
-#
+    
+    for u in vertices:
+        if visitado[u] == False:
+            tempo = DFS_Visit(gTransposto, u, visitado, tempoVisita, antecessor, tempoFinal, tempo)
 
 # Vetor C/Visitado
 # Vetor T/tempo de visita
 # Vetor A'/Antecessor 
 # Vetor F/ tempo que é finalizado
-    
-visitado = {x:False for x in g.vertices} # Cv←false ∀v ∈ V
-tempoVisita = {x:float('inf') for x in g.vertices} # Tv ←∞ ∀v ∈ V
-tempoFinal = {x:float('inf') for x in g.vertices} # Fv ←∞ ∀v ∈ V
-antecessor = {x:None for x in g.vertices} # Av ←∞ ∀v ∈ V
+
+visitado, tempoVisita, antecessor, tempoFinal = reset()
 DFS(g, visitado, tempoVisita, antecessor, tempoFinal)
-print(visitado)
 auxiliar = copy.deepcopy(g)
 auxiliar.arestas = g.transposta()
 #print(id(g))
 #print(id(auxiliar))
-print(g.arestas)
-print(auxiliar.arestas)
-DFS_Alterado(auxiliar, visitado, tempoVisita, antecessor, tempoFinal)
+#print(g.arestas)
+#print(auxiliar.arestas)
+#print(visitado)
+print(g.vertices)
+print(tempoVisita)
+print(tempoFinal)
+vertices_ordenados = sorted(g.vertices, key=lambda e: tempoFinal[e], reverse=True)
+print(vertices_ordenados)
+DFS_Alterado(auxiliar, vertices_ordenados, *reset())
 
 # Saida
 #saidaSafe = [[]]
